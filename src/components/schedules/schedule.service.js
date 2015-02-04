1dc365adb630ecf7c2033027138862fc6bb95029
femi oni <freefony@gmail.com>
2015-02-04 10:18:12 +0100

    replace user with Authuser in schedule service

--- src/components/schedules/schedule.service.js
+++ src/components/schedules/schedule.service.js
@@ -4,10 +4,10 @@
 'use strict';
 
 angular.module('schedules')
-  .service('scheduleService', function(user, dbService, couchUtil, utility) {
+  .service('scheduleService', function(AuthService, dbService, couchUtil, utility) {
 
     this.all = function() {
-      var params = couchUtil.key(user.email + '-' + utility.formatDate(new Date()));
+      var params = couchUtil.key(AuthService.currentUser.name + '-' + utility.formatDate(new Date()));
       /*eslint-disable camelcase */
       params.include_docs = true;
       /*eslint-enable camelcase */
